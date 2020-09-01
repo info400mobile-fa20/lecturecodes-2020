0d5515e6949527e2ef0021a5b5432713dd1d14a3
Christina <sweetflute@gmail.com>
2020-08-31 21:19:09 -0400

    update js-1 solution

--- 02/js-1/js/index.js
+++ 02/js-1/js/index.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 	//We will work on the first half of the exercise in the class here
 	document.getElementById("menu1").addEventListener("click", function(){
-		alert(document.getElementById("paragraph1").textContent = "text");
+		alert(document.getElementById("paragraph1").textContent);
 	});
 
 	document.getElementById("menu2").addEventListener("click", function(){
@@ -20,4 +20,29 @@ document.addEventListener("DOMContentLoaded", function(event) {
 		alert(document.getElementById("img1").getAttribute("src"));
 	});
 	//Your exercise start here
+
+	// document.getElementById("test-menu1").addEventListener("click", function(){
+	// 	alert(document.getElementById("test").textContent);
+	// });
+
+	document.querySelector("#test-menu1").addEventListener("click", function(){
+		alert(document.querySelector("#test").textContent);
+	});
+
+	document.getElementById("test-menu2").addEventListener("click", function(){
+		alert(document.getElementById("test").innerHTML);
+	});
+
+	document.getElementById("test-menu3").addEventListener("click", function(){
+		alert(document.getElementById("url-test").getAttribute("href"));
+	});
+
+	document.getElementById("test-menu4").addEventListener("click", function(){
+		alert(document.getElementById("img-test").getAttribute("src"));
+	});
+
+	document.getElementById("test-menu5").addEventListener("click", function(){
+		alert(document.getElementById("button-test").value);
+	});
+
 });
