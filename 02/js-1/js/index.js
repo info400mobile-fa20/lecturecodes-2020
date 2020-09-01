572f3b87d84f8cb88249d31c401ea9d72aefc86d
Christina <sweetflute@gmail.com>
2020-08-31 21:17:05 -0400

    update lecture exercise

--- 02/js-1/js/index.js
+++ 02/js-1/js/index.js
@@ -1,5 +1,23 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 	//We will work on the first half of the exercise in the class here
+	document.getElementById("menu1").addEventListener("click", function(){
+		alert(document.getElementById("paragraph1").textContent = "text");
+	});
 
+	document.getElementById("menu2").addEventListener("click", function(){
+		alert(document.getElementById("paragraph2").innerHTML);
+	});
+
+	document.getElementById("menu3").addEventListener("click", function(){
+		alert(document.getElementById("textfield1").value);
+	});
+
+	document.getElementById("menu4").addEventListener("click", function(){
+		alert(document.getElementById("url1").getAttribute("href"));
+	});
+
+	document.getElementById("menu5").addEventListener("click", function(){
+		alert(document.getElementById("img1").getAttribute("src"));
+	});
 	//Your exercise start here
 });
